6f4b7cebc3ae17467100e261718fb158ee166dcb
Bennet Rupnow <61080947+M1st3rButt3r@users.noreply.github.com>
2021-02-28 16:44:27 +0100

    commented

--- public/contextmenu.js
+++ public/contextmenu.js
@@ -1,16 +1,19 @@
 var currentMenu = null
 var menuData = -1;
 
+//activates / deactivates the menu  
 const toggleMenu = command => {
     currentMenu.style.display = command === "show" ? "block" : "none";
 };
 
+//sets the position of the menu
 const setPosition = ({ top, left }) => {
     currentMenu.style.left = `${left}px`;
     currentMenu.style.top = `${top}px`;
     toggleMenu("show");
 };
 
+//activates the menu on right click
 function activateMenu(menu)
 {
     if(currentMenu)
